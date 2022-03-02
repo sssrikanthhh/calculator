9f70f68996673efe1486c273ef2be320cd70885c
srikanth <chiramanasrikanth265@gmail.com>
2022-03-02 16:44:26 +0530

    complete basic math functions

---
+++ main.js
@@ -0,0 +1,9 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+console.log(add(2, 2));
+console.log(subtract(4, 2));
+console.log(multiply(2, 2));
+console.log(divide(8, 2));
